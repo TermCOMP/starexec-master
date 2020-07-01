2cb627bdb928c7ce69ca514d22ebff579f3b4317
Akihisa Yamada <ayamada@Akihisas-MacBook-Pro.local>
2020-07-01 10:04:34 +0900

    adjusted past info

--- Y2018_info.php
+++ Y2018_info.php
@@ -1,147 +1,145 @@
 <h1>Termination Competition 2018</h1>
 <?php
-$competition = [
-	'name' => 'TermCOMP 2018',
-	'mcats' => [
-		"Termination of Rewriting" => [
-			'TRS Standard' => [
-				'type' => 'termination',
-				'jobid' => 30034,
-			],
-			'TRS Standard Certified' => [
-				'type' => 'termination',
-				'jobid' => 30038,
-			],
-			'SRS Standard' => [
-				'type' => 'termination',
-				'jobid' => 30035,
-			],
-			'SRS Standard Certified' => [
-				'type' => 'termination',
-				'jobid' => 30039,
-			],
-			'TRS Relative' => [
-				'type' => 'termination',
-				'jobid' => 30036,
-			],
-			'TRS Relative Certified' => [
-				'type' => 'termination',
-				'jobid' => 30040,
-			],
-			'SRS Relative' => [
-				'type' => 'termination',
-				'jobid' => 30037,
-			],
-			'SRS Relative Certified' => [
-				'type' => 'termination',
-				'jobid' => 30041,
-			],
-			'TRS Equational' => [
-				'type' => 'termination',
-				'jobid' => 30042,
-			],
-			'TRS Equational Certified' => [
-				'type' => 'termination',
-				'jobid' => 30043,
-			],
-			'TRS Conditional' => [
-				'type' => 'termination',
-				'jobid' => 30044,
-			],
-			'TRS Context Sensitive' => [
-				'type' => 'termination',
-				'jobid' => 30045,
-			],
-			'TRS Innermost' => [
-				'type' => 'termination',
-				'jobid' => 30046,
-			],
-			'HRS (union beta)' => [
-				'type' => 'termination',
-				'jobid' => 30047,
-			],
+$name = 'TermCOMP 2018';
+$raw_mcats = [
+	"Termination of Rewriting" => [
+		'TRS Standard' => [
+			'type' => 'termination',
+			'jobid' => 30034,
 		],
-	 	"Termination of Programs" => [
-			'C' => [
-				'type' => 'termination',
-				'jobid' => 30048,
-			],
-			'C Integer' => [
-				'type' => 'termination',
-				'jobid' => 30049,
-			],
-			'Integer Transition Systems' => [
-				'type' => 'termination',
-				'jobid' => 30050,
-			],
-			'Integer TRS Innermost' => [
-				'type' => 'termination',
-				'jobid' => 30051,
-			],
+		'TRS Standard Certified' => [
+			'type' => 'termination',
+			'jobid' => 30038,
 		],
-		"Complexity Analysis" => [
-			'Complexity: ITS' => [
-				'type' => 'complexity',
-				'jobid' => 30054,
-			],
-			'Complexity: C Integer' => [
-				'type' => 'complexity',
-				'jobid' => 30055,
-			],
-			'Runtime Complexity: TRS' => [
-				'type' => 'complexity',
-				'jobid' => 30091,
-			],
-			'Runtime Complexity: TRS Innermost' => [
-				'type' => 'complexity',
-				'jobid' => 30092,
-			],
-			'Runtime Complexity: TRS Innermost Certified' => [
-				'type' => 'complexity',
-				'jobid' => 30094,
-			],
+		'SRS Standard' => [
+			'type' => 'termination',
+			'jobid' => 30035,
 		],
-		"Demonstration" => [
-			'TRS Outermost' => [
-				'type' => 'termination',
-				'jobid' => 30096,
-			],
-			'TRS Outermost Certified' => [
-				'type' => 'termination',
-				'jobid' => 30098,
-			],
-			'TRS Innermost Certified' => [
-				'type' => 'termination',
-				'jobid' => 30097,
-			],
-			'HRS' => [
-				'type' => 'termination',
-				'jobid' => 30099,
-			],
-			'Java Bytecode' => [
-				'type' => 'termination',
-				'jobid' => 30100,
-			],
-			'Prolog' => [
-				'type' => 'termination',
-				'jobid' => 30101,
-			],
-			'Haskell' => [
-				'type' => 'termination',
-				'jobid' => 30102,
-			],
-			'Derivational Complexity: TRS' => [
-				'type' => 'complexity',
-				'jobid' => 30103,
-			],
-			'Derivational Complexity: TRS Certified' => [
-				'type' => 'complexity',
-				'jobid' => 30104,
-			],
-			'Runtime Complexity: TRS Certified' => [
-				'type' => 'complexity',
-				'jobid' => 30105,
-			],
+		'SRS Standard Certified' => [
+			'type' => 'termination',
+			'jobid' => 30039,
+		],
+		'TRS Relative' => [
+			'type' => 'termination',
+			'jobid' => 30036,
+		],
+		'TRS Relative Certified' => [
+			'type' => 'termination',
+			'jobid' => 30040,
+		],
+		'SRS Relative' => [
+			'type' => 'termination',
+			'jobid' => 30037,
+		],
+		'SRS Relative Certified' => [
+			'type' => 'termination',
+			'jobid' => 30041,
+		],
+		'TRS Equational' => [
+			'type' => 'termination',
+			'jobid' => 30042,
+		],
+		'TRS Equational Certified' => [
+			'type' => 'termination',
+			'jobid' => 30043,
+		],
+		'TRS Conditional' => [
+			'type' => 'termination',
+			'jobid' => 30044,
+		],
+		'TRS Context Sensitive' => [
+			'type' => 'termination',
+			'jobid' => 30045,
+		],
+		'TRS Innermost' => [
+			'type' => 'termination',
+			'jobid' => 30046,
+		],
+		'HRS (union beta)' => [
+			'type' => 'termination',
+			'jobid' => 30047,
+		],
+	],
+	"Termination of Programs" => [
+		'C' => [
+			'type' => 'termination',
+			'jobid' => 30048,
+		],
+		'C Integer' => [
+			'type' => 'termination',
+			'jobid' => 30049,
+		],
+		'Integer Transition Systems' => [
+			'type' => 'termination',
+			'jobid' => 30050,
+		],
+		'Integer TRS Innermost' => [
+			'type' => 'termination',
+			'jobid' => 30051,
+		],
+	],
+	"Complexity Analysis" => [
+		'Complexity: ITS' => [
+			'type' => 'complexity',
+			'jobid' => 30054,
+		],
+		'Complexity: C Integer' => [
+			'type' => 'complexity',
+			'jobid' => 30055,
+		],
+		'Runtime Complexity: TRS' => [
+			'type' => 'complexity',
+			'jobid' => 30091,
+		],
+		'Runtime Complexity: TRS Innermost' => [
+			'type' => 'complexity',
+			'jobid' => 30092,
+		],
+		'Runtime Complexity: TRS Innermost Certified' => [
+			'type' => 'complexity',
+			'jobid' => 30094,
+		],
+	],
+	"Demonstration" => [
+		'TRS Outermost' => [
+			'type' => 'termination',
+			'jobid' => 30096,
+		],
+		'TRS Outermost Certified' => [
+			'type' => 'termination',
+			'jobid' => 30098,
+		],
+		'TRS Innermost Certified' => [
+			'type' => 'termination',
+			'jobid' => 30097,
+		],
+		'HRS' => [
+			'type' => 'termination',
+			'jobid' => 30099,
+		],
+		'Java Bytecode' => [
+			'type' => 'termination',
+			'jobid' => 30100,
+		],
+		'Prolog' => [
+			'type' => 'termination',
+			'jobid' => 30101,
+		],
+		'Haskell' => [
+			'type' => 'termination',
+			'jobid' => 30102,
+		],
+		'Derivational Complexity: TRS' => [
+			'type' => 'complexity',
+			'jobid' => 30103,
+		],
+		'Derivational Complexity: TRS Certified' => [
+			'type' => 'complexity',
+			'jobid' => 30104,
+		],
+		'Runtime Complexity: TRS Certified' => [
+			'type' => 'complexity',
+			'jobid' => 30105,
 		],
 	],
 ];
--- Y2019_info.php
+++ Y2019_info.php
@@ -1,131 +1,129 @@
 <h1>Termination Competition 2019</h1>
 <?php
-$competition = [
-	"name" => "TermCOMP 2019",
-	"mcats" => [
-		"Termination of Rewriting" => [
-			'TRS Standard' => [
-				'type' => 'termination',
-				'jobid' => 33457,
-			],
-			'TRS Standard Certified' => [
-				'type' => 'termination',
-				'jobid' => 33116,
-			],
-			'SRS Standard' => [
-				'type' => 'termination',
-				'jobid' => 33458,
-			],
-			'SRS Standard Certified' => [
-				'type' => 'termination',
-				'jobid' => 33117,
-			],
-			'TRS Relative' => [
-				'type' => 'termination',
-				'jobid' => 33012,
-			],
-			'TRS Relative Certified' => [
-				'type' => 'termination',
-				'jobid' => 33126,
-			],
-			'SRS Relative' => [
-				'type' => 'termination',
-				'jobid' => 33461,
-			],
-			'SRS Relative Certified' => [
-				'type' => 'termination',
-				'jobid' => 33127,
-			],
-			'TRS Equational' => [
-				'type' => 'termination',
-				'jobid' => 33020,
-			],
-			'TRS Equational Certified' => [
-				'type' => 'termination',
-				'jobid' => 33128,
-			],
-			'TRS Conditional' => [
-				'type' => 'termination',
-				'jobid' => 33455,
-			],
-			'TRS Context Sensitive' => [
-				'type' => 'termination',
-				'jobid' => 33019,
-			],
-			'TRS Innermost' => [
-				'type' => 'termination',
-				'jobid' => 33453,
-			],
-			'HRS (union beta)' => [
-				'type' => 'termination',
-				'jobid' => 33452,
-			],
+$name = 'TermCOMP 2019';
+$raw_mcats = [
+	"Termination of Rewriting" => [
+		'TRS Standard' => [
+			'type' => 'termination',
+			'jobid' => 33457,
 		],
-	 	"Termination of Programs" => [
-			'C' => [
-				'type' => 'termination',
-				'jobid' => 33437,
-			],
-			'C Integer' => [
-				'type' => 'termination',
-				'jobid' => 33454,
-			],
-			'Logic Programming' => [
-				'type' => 'termination',
-				'jobid' => 33595,
-			],
-			'Integer Transition Systems' => [
-				'type' => 'termination',
-				'jobid' => 33456,
-			],
-			'Integer TRS Innermost' => [
-				'type' => 'termination',
-				'jobid' => 33016,
-			],
+		'TRS Standard Certified' => [
+			'type' => 'termination',
+			'jobid' => 33116,
 		],
-		"Complexity Analysis" => [
-			'Complexity: ITS' => [
-				'type' => 'complexity',
-				'jobid' => 33014,
-			],
-			'Complexity: C Integer' => [
-				'type' => 'complexity',
-				'jobid' => 33013,
-			],
+		'SRS Standard' => [
+			'type' => 'termination',
+			'jobid' => 33458,
 		],
-		"Demonstrations" => [
-			'TRS Outermost' => [
-				'type' => 'termination',
-				'jobid' => 33568,
-			],
-			'TRS Outermost Certified' => [
-				'type' => 'termination',
-				'jobid' => 33569,
-			],
-			'TRS Innermost Certified' => [
-				'type' => 'termination',
-				'jobid' => 33570,
-			],
-			'Java Bytecode' => [
-				'type' => 'termination',
-				'jobid' => 33571,
-			],
-			'Haskell' => [
-				'type' => 'termination',
-				'jobid' => 33572,
-			],
-			'Runtime Complexity: TRS' => [
-				'type' => 'complexity',
-				'jobid' => 33563,
-			],
-			'Runtime Complexity: TRS Innermost' => [
-				'type' => 'complexity',
-				'jobid' => 33566,
-			],
-			'Runtime Complexity: TRS Innermost Certified' => [
-				'type' => 'complexity',
-				'jobid' => 33567,
-			],
+		'SRS Standard Certified' => [
+			'type' => 'termination',
+			'jobid' => 33117,
+		],
+		'TRS Relative' => [
+			'type' => 'termination',
+			'jobid' => 33012,
+		],
+		'TRS Relative Certified' => [
+			'type' => 'termination',
+			'jobid' => 33126,
+		],
+		'SRS Relative' => [
+			'type' => 'termination',
+			'jobid' => 33461,
+		],
+		'SRS Relative Certified' => [
+			'type' => 'termination',
+			'jobid' => 33127,
+		],
+		'TRS Equational' => [
+			'type' => 'termination',
+			'jobid' => 33020,
+		],
+		'TRS Equational Certified' => [
+			'type' => 'termination',
+			'jobid' => 33128,
+		],
+		'TRS Conditional' => [
+			'type' => 'termination',
+			'jobid' => 33455,
+		],
+		'TRS Context Sensitive' => [
+			'type' => 'termination',
+			'jobid' => 33019,
+		],
+		'TRS Innermost' => [
+			'type' => 'termination',
+			'jobid' => 33453,
+		],
+		'HRS (union beta)' => [
+			'type' => 'termination',
+			'jobid' => 33452,
+		],
+	],
+	"Termination of Programs" => [
+		'C' => [
+			'type' => 'termination',
+			'jobid' => 33437,
+		],
+		'C Integer' => [
+			'type' => 'termination',
+			'jobid' => 33454,
+		],
+		'Logic Programming' => [
+			'type' => 'termination',
+			'jobid' => 33595,
+		],
+		'Integer Transition Systems' => [
+			'type' => 'termination',
+			'jobid' => 33456,
+		],
+		'Integer TRS Innermost' => [
+			'type' => 'termination',
+			'jobid' => 33016,
+		],
+	],
+	"Complexity Analysis" => [
+		'Complexity: ITS' => [
+			'type' => 'complexity',
+			'jobid' => 33014,
+		],
+		'Complexity: C Integer' => [
+			'type' => 'complexity',
+			'jobid' => 33013,
+		],
+	],
+	"Demonstrations" => [
+		'TRS Outermost' => [
+			'type' => 'termination',
+			'jobid' => 33568,
+		],
+		'TRS Outermost Certified' => [
+			'type' => 'termination',
+			'jobid' => 33569,
+		],
+		'TRS Innermost Certified' => [
+			'type' => 'termination',
+			'jobid' => 33570,
+		],
+		'Java Bytecode' => [
+			'type' => 'termination',
+			'jobid' => 33571,
+		],
+		'Haskell' => [
+			'type' => 'termination',
+			'jobid' => 33572,
+		],
+		'Runtime Complexity: TRS' => [
+			'type' => 'complexity',
+			'jobid' => 33563,
+		],
+		'Runtime Complexity: TRS Innermost' => [
+			'type' => 'complexity',
+			'jobid' => 33566,
+		],
+		'Runtime Complexity: TRS Innermost Certified' => [
+			'type' => 'complexity',
+			'jobid' => 33567,
 		],
 	],
 ];
