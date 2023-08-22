ac01f7c41a1b9f64006cd4121d7a9269f3cc28d6
Akihisa Yamada <ayamada@trs.cm.is.nagoya-u.ac.jp>
2023-08-22 13:57:31 +0200

    visual

--- createjob.php
+++ createjob.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-</script>
+<style>
+ label { display: inline-block; }
+ input[type=number] { width: 8em; }
+</style>
 </head>
 <body>
 <?php
@@ -34,23 +37,23 @@ echo
  action="https://www.starexec.org/starexec/secure/add/job"
  target="_blank">
     <input type="submit" value="Create Job">
-    <input type="text" name="name" value="'. $job['name'] . '" style="width:80%"><br>
-    queue: <input type="number" name="queue" value='.$queue.'>
-    sid: <input type="number" name="sid" value='.$cat['spaceid'].'>
-    desc: <input type="text" name="desc"><br>
+    <input type="text" name="name" value="'. $job['name'] . '" style="width:30em;">
+    <label>desc: <input type="text" name="desc"></label>
+    <br>
+    <label>queue: <input type="number" name="queue" value='.$queue.'></label>
+    <label>sid: <input type="number" name="sid" value='.$cat['spaceid'].'></label>
     <select name="benchmarkingFramework">
         <option value="BENCHEXEC" selected="selected">BenchExec</option>
 	    <option value="RUNSOLVER">runsolver</option>
     </select>
+    <label>pause: <input checked type="radio" name="pause" value="yes">yes</label>
+    <label><input type="radio" name="pause" value="no" id="pause_no">no</label>
+    <br>
     <input type="hidden" name="preProcess" value="-1">
     <input type="hidden" name="seed" value="0">
-    cpuTimeout: <input type="number" name="cpuTimeout" value="'.$cpu_timeout.'">
-    wallclockTimeout: <input type="number" name="wallclockTimeout" value="'.$timeout.'">
-    maxMem: <input type="number" name="maxMem" value="'.$max_memory.'"><br>
-    pause: <input checked type="radio" name="pause" value="yes" id="pause_yes">
-    <label for="pause_yes">yes</label>
-    <input type="radio" name="pause" value="no" id="pause_no">
-    <label for="pause_no">no</label>
+    <label>cpuTimeout: <input type="number" name="cpuTimeout" value="'.$cpu_timeout.'"></label>
+    <label>wallclockTimeout: <input type="number" name="wallclockTimeout" value="'.$timeout.'"></label>
+    <label>maxMem: <input type="number" name="maxMem" value="'.$max_memory.'"></label>
     <input type="hidden" name="runChoice" value="choose">
     <select name="benchChoice">
         <option selected value="runAllBenchInHierarchy">runAllBenchInHierarchy</option>
@@ -66,12 +69,14 @@ echo
     <input type="hidden" name="saveOtherOutput" value="false">
     <input type="hidden" name="killDelay" value="0">
     <input type="hidden" name="softTimeLimit" value="0">
-    postProcess: <input type="number" name="postProcess" value='. $job['postproc'] . '><br>
-    Participants:
+    <label>postProcess: <input type="number" name="postProcess" value='. $job['postproc'] . '></label>
+    <br>
 ';
 				foreach( $job['participants'] as $partname => $configid ) {
 					echo
-'   '. $partname .': <input type="number" name="configs" value='. $configid . '>;
+'   <label>'. $partname .':
+     <input type="number" name="configs" value='. $configid . '>
+    </label>
 ';
 				}
 				echo
