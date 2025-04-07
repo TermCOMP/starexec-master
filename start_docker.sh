f6f2dd76bb5b8d0acaab30d8f2d5c0d018d2cf24
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-04-07 17:48:44 +0200

    fix path

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -v .:/var/www/html -d -p 2222:80 --name=termcomp --rm -t termcomp
+sudo docker run -v ~/termcomp/starexec-master:/var/www/html -d -p 2222:80 --name=termcomp --rm -t termcomp
