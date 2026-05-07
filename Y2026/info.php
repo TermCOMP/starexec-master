d5125306cdf900f4d53090e34a71c21e6fccf335
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2026-05-07 12:41:06 +0200

    add config file for 2026

---
+++ Y2026/info.php
@@ -0,0 +1,25 @@
+<?php
+$title = 'Termination Competition 2026';
+$shortname = 'TermCOMP 2026';
+$showconfig = true;
+$showscore = false;
+$note = '';
+$db = 'TPDB 11.5';
+$closed = false; // make true when registration is closed.
+$previous = 'Y2025';
+
+$categories = [
+	'Termination of Rewriting' => [
+                'nti' => [
+			'type' => 'termination',
+			'dir' => 'TRS_Standard',
+			'id' => 275763,
+			'participants' => [
+				'NTI' => 783008,
+			],
+			'certified' => [
+			],
+		],
+	],
+];
+?>
